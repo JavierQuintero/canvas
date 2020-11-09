461ebe86abd401d39e8c67c908a0a38f7ed77ee6
Javi <ejviquin@gmail.com>
2020-11-09 08:28:55 -0500

    Se separan los canvas y se aniade parametro a la funcion de dibujarLinea

--- dibujo.js
+++ dibujo.js
@@ -1,35 +1,44 @@
-let d = document.getElementById("dibujito");
-let lienzo = d.getContext("2d");
+let $img1 = document.getElementById("img1");
+let $img2 = document.getElementById("img2");
+let $img3 = document.getElementById("img3");
+let $img4 = document.getElementById("img4");
+let $img5 = document.getElementById("img5");
+let $img6 = document.getElementById("img6");
+let lienzo1 = $img1.getContext("2d");
+let lienzo2 = $img2.getContext("2d");
+let lienzo3 = $img3.getContext("2d");
+let lienzo4 = $img4.getContext("2d");
+let lienzo5 = $img5.getContext("2d");
+let lienzo6 = $img6.getContext("2d");
+// for(i=1;i<=6;i++){
+// 	let $img1 = document.getElementById("img");
+// 	console.log(i)
+// }
 
-// dibujarLineas('#4ad', 20, 300, 0, 20)
-// dibujarLineas('#4ad', 30, 300, 0, 30)
-// dibujarLineas('#4ad', 40, 300, 0, 40)
-// dibujarLineas('#4ad', 50, 300, 0, 50)
-// dibujarLineas('#4ad', 60, 300, 0, 60)
-// dibujarLineas('#4ad', 70, 300, 0, 70)
-// dibujarLineas('#4ad', 80, 300, 0, 80)
+for( i=-1, a=30; i<=30, a>=0; i++,a--){
 
-for( i=0, a=30; i<=30, a>=0; i++,a-- ){
-	dibujarLineas('#4ad', (i+1)*10, 300, 0, (i+1)*10)
-	debugger
-	dibujarLineas('#c2e', 300, (i+1)*10, a*10 ,300)
-	dibujarLineas('#0d1', 0, a*10, (i+1)*10, 0)
-	dibujarLineas('#010', (i+1)*10, 0, 300, (i+1)*10)
+	dibujarLinea(lienzo1, "red", 300, (i+1)*10, 0, a*10)
+	dibujarLinea(lienzo2, "gray", (i+1)*10, 0, a*10, 300)
+	dibujarLinea(lienzo3, '#010', (i+1)*10, 0, 300, (i+1)*10)
+	dibujarLinea(lienzo4, '#4ad', (i+1)*10, 300, 0, (i+1)*10)
+	dibujarLinea(lienzo5, '#c2e', 300, (i+1)*10, a*10 ,300)
+	dibujarLinea(lienzo6, '#0d1', 0, a*10, (i+1)*10, 0)
 }
 //Dibujando Texto
-lienzo.font = '20px arial';
-lienzo.fillStyle = 'cyan';
-lienzo.fillText("devJaviQ", 100, 150)
+
+lienzo1.font = '2.3em cursive';
+lienzo1.fillStyle = 'rgba(0,0,0,0.6)';
+lienzo1.fillText("devJaviQ", 80, 290)
 
 //funcion Dibujo en canvas
 
-function dibujarLineas(color, x_init, y_init, x_end, y_end){
+function dibujarLinea(e,color, x_init, y_init, x_end, y_end){
 
-	lienzo.beginPath();
-	lienzo.strokeStyle = color;
-	lienzo.moveTo(x_init, y_init);
-	lienzo.lineTo(x_end, y_end);
-	lienzo.stroke();
-	lienzo.closePath();
+	e.beginPath();
+	e.strokeStyle = color;
+	e.moveTo(x_init, y_init);
+	e.lineTo(x_end, y_end);
+	e.stroke();
+	e.closePath();
 
 }
